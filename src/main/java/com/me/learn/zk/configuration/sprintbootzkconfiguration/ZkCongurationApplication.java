b4567e16df3f36cd4b06d38ef8681904ad93c651
Jed Li <Jed.Li@ariix.com>
2021-08-19 15:49:47 +0800

    Initial commit

---
+++ src/main/java/com/me/learn/zk/configuration/sprintbootzkconfiguration/ZkCongurationApplication.java
@@ -0,0 +1,13 @@
+package com.me.learn.zk.configuration.sprintbootzkconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkCongurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkCongurationApplication.class, args);
+    }
+
+}
